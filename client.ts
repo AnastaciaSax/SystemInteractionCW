d24e8e3ec372adf66afec5fca6bda95a18f65bd5
AnastaciaSax <nastya.saks2016@yandex.com>
2026-01-09 08:59:32 +0300

    fix: simple client

--- client.ts
+++ client.ts
@@ -4,8 +4,8 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-// Просто создаем PrismaClient без дополнительных опций
-// Он автоматически возьмет DATABASE_URL из .env
 export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
